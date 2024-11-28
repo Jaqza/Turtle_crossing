98150ed38d8f217f82227c8506917c8ac84c54ff
Paweł <mielczarekpawel26@gmail.com>
2024-11-28 20:38:03 +0100

    movement and levels works fine

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.bgcolor("gray")
+screen.listen()
 screen.tracer(0)
 
+player = Player()
+scoreboard = Scoreboard()
+screen.onkey(player.move_forward, "w")
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    if player.ycor() >= 280:
+        player.go_on_start()
+        scoreboard.level_up()
+
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,18 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 0
+        self.color("black")
+        self.hideturtle()
+        self.penup()
+        self.goto(-230, 270)
+        self.level_up()
+
+    def level_up(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level:{self.level}", False, "center", font=FONT)
