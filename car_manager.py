fb8d962b1b8c45cda1f89583461d5efb561a630d
Paweł <mielczarekpawel26@gmail.com>
2024-11-29 23:01:23 +0100

    I understand a litle bit more of the classes.I have made a little mistake in cr manager but at the end it was valuable lesson.

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,34 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+from turtle import Turtle
+from random import randint, choice
+COLORS = ["red", "orange", "yellow", "black", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def creating_cars(self):
+        chance = randint(1, 6)
+        if chance == 1:
+            paint = choice(COLORS)
+            generated_y = randint(-260, 260)
+            car = Turtle("square")
+            car.penup()
+            car.shapesize(1, 2)
+            car.shape()
+            car.color(paint)
+            self.cars.append(car)
+            car.goto(300, generated_y)
+
+    def traffic(self):
+        for car in self.cars:
+            car.back(self.car_speed)
+
+    def run_over(self):
+        self.car_speed = 0
+
+    def car_speed_up(self):
+        self.car_speed += MOVE_INCREMENT
--- main.py
+++ main.py
@@ -9,22 +9,28 @@ screen.setup(width=600, height=600)
 screen.bgcolor("gray")
 screen.listen()
 screen.tracer(0)
-
 player = Player()
 scoreboard = Scoreboard()
 screen.onkey(player.move_forward, "w")
+car_manager = CarManager()
+
 game_is_on = True
+
 while game_is_on:
+    car_manager.creating_cars()
     time.sleep(0.1)
+    car_manager.traffic()
+
     screen.update()
 
     if player.ycor() >= 280:
         player.go_on_start()
         scoreboard.level_up()
+        car_manager.car_speed_up()
 
-    if scoreboard.level == 2:
-        """CHANGE IT WHEN THE COLLISION OCCUR"""
-        scoreboard.game_over()
-        game_is_on = False
-
+    for car in car_manager.cars:
+        if car.distance(player) < 22:
+            car_manager.run_over()
+            scoreboard.game_over()
+            game_is_on = False
 screen.exitonclick()
